2ed9e74d47ffca3fde39e72de8d90cd74a9c81fb
wobey000 <wobey96@gmail.com>
2025-11-04 12:03:38 -0700

    Add project files.

---
+++ DirectXFantasy/DirectXFantasy.cpp
@@ -0,0 +1,12 @@
+﻿// DirectXFantasy.cpp : Defines the entry point for the application.
+//
+
+#include "DirectXFantasy.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake This is DirectXFantasy" << endl;
+	return 0;
+}
